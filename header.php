996259d424766de77c46e6b3aade3853601a5fb6
Anwrs <54798838+Anwrs@users.noreply.github.com>
2020-03-19 14:59:13 +0100

    First commit

---
+++ header.php
@@ -0,0 +1,62 @@
+<style>
+    <?php
+    include 'css/main.css';
+    ?>
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <title>Welkom op Redgame!</title>
+</head>
+<body>
+
+    <header>
+        <div class="nav">
+            <div class="navimg">
+                <img src="logo.png" alt="Logo">
+            </div>
+
+            <div class="dropdown">
+                <button class="dropi"><i class="fas fa-bars"></i></button>
+                <div class="dropdown-content">
+                    <a href="#"><i class="fas fa-ghost"></i>Games</a>
+                    <a href="#"><i class="fas fa-comments"></i>Forum</a>
+                    <a href="#"><i class="fas fa-address-book"></i>Contact</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <script src="https://kit.fontawesome.com/82664ff85a.js" crossorigin="anonymous"></script>
+</body>
+</html>
+<!-- 
+/* .kleur1 {
+    background-color: #FC9335;
+    width: 100px;
+    height: 100px;
+}
+.kleur2 {
+    background-color: #6D6D6D;
+    width: 100px;
+    height: 100px;
+}
+.kleur3 {
+    background-color: #1D1D1D;
+    width: 100px;
+    height: 100px;
+}
+.kleur4 {
+    background-color: #F2F2F2;
+    width: 100px;
+    height: 100px;
+}
+.kleur5 {
+    background-color: #F1E7DD;
+    width: 100px;
+    height: 100px;
+} */ -->
