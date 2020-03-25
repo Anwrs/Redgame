f9b43a91320f555503a0889d87433f6a1fbaecf8
Anwrs <anouar.elghazaoui@student.rocva.nl>
2020-03-25 14:18:27 +0100

    Voorpagina Afgerond

--- header.php
+++ header.php
@@ -1,32 +1,25 @@
-<style>
-    <?php
-    include 'css/main.css';
-    ?>
-</style>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-    <title>Welkom op Redgame!</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 
     <header>
         <div class="nav">
             <div class="navimg">
-                <img src="logo.png" alt="Logo">
+                <a href="home.php"><img src='images/cover1.png' alt="cover-logo"></a>
             </div>
 
-            <div class="dropdown">
-                <button class="dropi"><i class="fas fa-bars"></i></button>
-                <div class="dropdown-content">
-                    <a href="#"><i class="fas fa-ghost"></i>Games</a>
-                    <a href="#"><i class="fas fa-comments"></i>Forum</a>
-                    <a href="#"><i class="fas fa-address-book"></i>Contact</a>
-                </div>
+            <div class="navbar">
+                <nav class="bar">
+                    <a href="#"><i class="fas fa-gamepad"></i> GAMES</a>
+                    <a href="#"><i class="far fa-file-alt"></i> FORUM</a>
+                    <a href="#"><i class="far fa-comment"></i> CONTACT</a>
+                </nav>
             </div>
         </div>
     </header>
--- home.php
+++ home.php
@@ -1,68 +1,86 @@
-<style>
-    <?php
-    include 'css/main.css';
-    include 'header.php';
-    ?>
-</style>
+<?php
+include 'header.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
-    <title></title>
+    <link rel="stylesheet" href="css/main.css">
+    <title>Welkom op redgame!</title>
 </head>
+
 <body>
-    <main>
-        <div class="gamebar">
-            <div class="gamebalk">
-                <a href="">
-                <img class="roundimg" src="mc.jpg" alt="Game Foto">
-                <h2>Minecraft</h2>
-                </a>
-            </div>
-            <div class="gamebalk">
-                <a href="">
-                <img class="roundimg" src="lol.jpg" alt="Game Foto">
-                <h2>League of Legends</h2>
-                </a>
-            </div>
-            <div class="gamebalk">
-            <a href="">
-                <img class="roundimg" src="rs.jpg" alt="Game Foto">
-                <h2>Rainbow Six Siege</h2>
-                </a>
-            </div>
-            <div class="gamebalk">
-            <a href="">
-                <img class="roundimg" src="gta.jpg" alt="Game Foto">
-                <h2>Grand Theft Auto V</h2>
-                </a>
-            </div>
-            <div class="gamebalk">
-            <a href="">
-                <img class="roundimg" src="cs.jpg" alt="Game Foto">
-                <h2>CS:GO</h2>
-                </a>
-            </div>
-            <div class="gamebalk">
-            <a href="">
-                <img class="roundimg" src="tw.jpg" alt="Game Foto">
-                <h2>The Witcher 3</h2>
-                </a>
-            </div>
+    <main class="main-home">
+
+        <div class="main-text">
+            <h1>
+                <span style="font-size: 50px; display:flex; align-items:center; justify-content:center;">
+                    Welkom op<img style="width:200px; position:relative; left: 15px;" src="images/coverredgame.png"
+                        alt="redgame">
+                </span><br>
+                Op onze website kun je met andere gedediceerde gamers praten over jou favoriete spel!
+                Je kan het hebben over tactieken, updates, nieuwe meta's en jou ervaringen over de game.
+            </h1>
         </div>
 
-        <div class="mainblock">
-            <div class="maincover">
-                <h1>Welkom op de website!</h1>
-                <h2>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, provident 
-                ea laudantium ullam possimus, suscipit est quod excepturi fugiat illo ad 
-                nobis explicabo sapiente veritatis culpa! Obcaecati, vero. Labore, quasi!
-                </h2>
-            </div>
+        <div class="main-games">
+    <?php 
+        $mc = '<div  class="gamebox">
+        <a href="">
+        <img src="images/mc.jpg" alt="game">
+        Minecraft
+        </a>
+        </div>';
+    
+        $gta = '<div  class="gamebox">
+        <a href="">
+        <img src="images/gta.jpg" alt="game">
+        Gta
+        </a>
+        </div>';
+
+        $rs = '<div  class="gamebox">
+        <a href="">
+        <img src="images/rs.jpg" alt="game">
+        R6S
+        </a>
+        </div>';
+
+        $lol = '<div  class="gamebox">
+        <a href="">
+        <img src="images/lol.jpg" alt="game">
+        League of Legends
+        </a>
+        </div>';
+
+        $cs = '<div  class="gamebox">
+        <a href="">
+        <img src="images/cs.jpg" alt="game">
+        CS:GO
+        </a>
+        </div>';
+
+        $tw = '<div  class="gamebox">
+        <a href="">
+        <img src="images/tw.jpg" alt="game">
+        The witcher
+        </a>
+        </div>';
+
+        $ar = array($mc,$rs,$gta,$cs,$tw,$lol);
+        shuffle($ar);
+
+        echo ($ar[0]);
+        echo ($ar[1]);
+        echo ($ar[2]);
+        echo ($ar[3]);
+        echo ($ar[4]);
+        echo ($ar[5]);
+        ?>
         </div>
     </main>
 </body>
