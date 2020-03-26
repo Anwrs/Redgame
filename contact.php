3f1eaa3376fd39ae07db500f2a8387382b9fd208
Anwrs <anouar.elghazaoui@student.rocva.nl>
2020-03-26 12:43:01 +0100

    push

--- contact.php
+++ contact.php
@@ -8,9 +8,9 @@ include 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/contact.css">
-    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Contact us!</title>
 </head>
 
 <body>
