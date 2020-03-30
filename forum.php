f83fd55c7693608ddde00edc0511de068e8966c9
daan <2087884@talnet.nl>
2020-03-30 14:39:55 +0200

    ugh

--- forum.php
+++ forum.php
@@ -1,5 +1,22 @@
 <?php
 include 'header.php';
+$host = 'projects.bit-academy.nl:3306';
+$db   = 'redgame';
+$user = 'redgame';
+$pass = 'TVvWpPhGLv7VGhuF';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
 ?>
 
 <!DOCTYPE html>
