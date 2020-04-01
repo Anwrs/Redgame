2ab3465dfdcbcb46b4c305f1c8c0148291271cc5
Mark Verrips <2083591@talnet.nl>
2020-04-01 16:06:14 +0200

    iets

---
+++ games.php
@@ -0,0 +1,47 @@
+<?php
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/games.css">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <div id="explanation">
+            Hier kunt u zien wat de games inhouden, als u op het logo klikt word u doorgestuurd naar de officiele site van de game
+     </div>
+    <div id="games">
+        <div id="row1">
+        <img src="images/lo.jpg" class="photo">
+        <p>Een moba</p>
+        <img src="images/cs.jpg" class="photo">
+        <p> wdefrdg</p>
+      </div>
+        <div id="row2">
+        <img src="images/tw.jpg" class="photo">
+        <p>efsgrdthf</p>
+        <img src="images/gtacover.jpg" class="photo">
+        <p>efsgrdthf</p>
+     </div>
+        <div id="row3">
+        <img src="images/mc.jpg" class="photo">
+        <p>efsgrdthf</p>
+        <img src="images/rs.jpg" class="photo">
+        <p>efsgrdthf</p>
+      </div>
+     </div>
+  </div>
+    
+    
+    
+    
+    </main>
+</body>
