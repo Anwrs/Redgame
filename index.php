c0ebedf1d3e6c549bf582efacd54a7dad3dfb273
daan <2087884@talnet.nl>
2020-04-02 12:06:23 +0200

    on naar to veranderd

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include 'header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
-    <title>Welcome on Redgame!</title>
+    <title>Welcome to Redgame!</title>
 </head>
 
 <body>
